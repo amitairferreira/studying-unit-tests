04cdf5a59ae4d9b27cbbcce9fcecdd31457e8735
amitairferreira <amitairlima@gmail.com>
2024-03-18 13:18:05 -0300

    Testing for errors

--- src/test/Util.test.ts
+++ src/test/Util.test.ts
@@ -9,19 +9,37 @@ describe('Util test suite', () => {
 
         beforeEach(() => {
             sut = new StringUtils();
-            console.log('Setup')
-        })
-
-        afterEach(() => {
-            // clearing moks
-            console.log('Teardown');
+            
         })
 
         it('Should return correct upperCase', () => {
             const actual = sut.toUpperCase('abc');
-
             expect(actual).toBe('ABC');
             console.log('Actual Test')
+        })
+
+        it('Should throw error on invalid argument - function', () => {
+            function expectError() {
+                const actual = sut.toUpperCase('');
+            }
+            expect(expectError).toThrow();
+        })
+
+        it('Should throw error on invalid argument - arrow function', () => {
+            expect(() => {
+                sut.toUpperCase('');
+            }).toThrowError('Invalid argument!');
+        })
+
+        it.only('Should throw error on invalid argument - try catch block', (done) => {
+           try {
+                sut.toUpperCase('');
+                done('GetStringInfo should throw error for invalid arg!')
+           } catch (error) {
+                expect(error).toBeInstanceOf(Error);
+                expect(error).toHaveProperty('message', 'Invalid argument!');
+                done();
+           }
         })
 
     });
