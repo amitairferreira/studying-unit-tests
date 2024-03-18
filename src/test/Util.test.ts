038287169c4aa5503928e1cbb134d3dcfe9e999f
amitairferreira <amitairlima@gmail.com>
2024-03-18 10:07:16 -0300

    Multiple tests structure

---
+++ src/test/Util.test.ts
@@ -0,0 +1,51 @@
+import { getStringInfo, toUpperCase } from "../app/Util";
+
+describe('Util test suite', () => {
+
+    it('Should return uppercase of valid string', () => {
+        // arrange (organizar)
+        const sut = toUpperCase;
+        const expected = 'ABC'
+
+        // act (ação)
+        const actual = sut('abc');
+
+        //assert (afirmação)
+        expect(actual).toBe(expected);
+    });
+
+
+    describe('getStringInfo for arg My-String should', () => {
+
+        test('return right length', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toHaveLength(9);
+        })
+        test('return right lower case', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.lowerCase).toBe('my-string');
+        });
+        test('return right upper case', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.upperCase).toBe('MY-STRING');
+        });
+        test('return right characters', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.characters).toEqual(['M', 'y', '-', 'S', 't', 'r', 'i', 'n', 'g']);
+            expect(actual.characters).toContain<string>('M');
+            expect(actual.characters).toEqual(
+                expect.arrayContaining(['S', 't', 'r', 'i', 'n', 'g', 'M', 'y', '-'])
+            ); 
+        });
+        test('return defined extra info', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toBeDefined();
+        });
+        test('return right extra info', () => {
+            const actual = getStringInfo('My-String');
+            expect(actual.extraInfo).toEqual({});
+        });
+        
+    });
+
+});
