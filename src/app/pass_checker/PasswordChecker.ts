eb3df9756b693c6b6b437ec9398189d7c7c4262e
amitairferreira <amitairlima@gmail.com>
2024-03-20 10:56:30 -0300

    TDD PassChecker Iteration 2

--- src/app/pass_checker/PasswordChecker.ts
+++ src/app/pass_checker/PasswordChecker.ts
@@ -2,20 +2,36 @@
 // tem comrprimento inferior a 8 caracteres
 // não tem letras maiúsculas
 // não tem letras minúsculas
+// Return motivos que invalidam a senha
 
+export enum PasswordErrors {
+   SHORT = 'Password is too short!',
+   NO_UPPER_CASE = 'Upper case letter required!',
+   NO_LOWER_CASE = 'Lower case letter required!'
+}
+
+export interface checkResult {
+    valid: boolean,
+    reasons: string[]
+}
 
 export class PasswordChecker {
 
-    public checkPassword(password: string): boolean {
+    public checkPassword(password: string): checkResult {
+        const reasons:PasswordErrors[] = [];
+
         if (password.length < 8) {
-            return false;
+          reasons.push(PasswordErrors.SHORT);
         }
         if (password == password.toLowerCase()) {
-            return false;
+            reasons.push(PasswordErrors.NO_UPPER_CASE);
         }
         if (password == password.toUpperCase()) {
-            return false;
+            reasons.push(PasswordErrors.NO_LOWER_CASE);
         }
-        return true;
+        return {
+            valid: reasons.length > 0 ? false : true,
+            reasons:reasons
+        };
     }
 }
