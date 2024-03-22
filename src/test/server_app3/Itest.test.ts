5c9f60a72e133bafa4300176448a104ab6c3405f
amitairferreira <amitairlima@gmail.com>
2024-03-22 16:58:11 -0300

    Multiple test configurations

--- src/test/server_app3/Itest.test.ts
+++ src/test/server_app3/Itest.test.ts
@@ -42,6 +42,7 @@ describe('Server app integration tests', ()=>{
 
         expect(result.status).toBe(HTTP_CODES.CREATED);
         expect(resultBody.userId).toBeDefined();
+        //console.log(`connectting to addres: ${process.env.HOST}`)
     });
 
     it('should register new user with awesomeRequest', async ()=>{
